cdfe72a3000339892c0bf6fff1b63be11fe2701a
vikram1804.prasad@gmail.com <vikram1804.prasad@gmail.com>
2017-08-20 21:02:50 +0530

    Add video list and video item component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyA-XD8r8K-UXVqKSkn-uGpSOv5Nws8V36k';
+
+// YTSearch({key:API_KEY, term : 'faydee'}, data => {console.log(data)});
 //Create a new component that produces HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = { videos : []};
+    YTSearch({key:API_KEY, term : 'faydee'}, videos => this.setState({ videos }));
+
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
