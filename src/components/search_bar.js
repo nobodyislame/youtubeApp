8b31ec829db32155f60967378b84fefb0959c151
Vikram <vikram.prasad@mountblue.io>
2017-08-23 11:48:17 +0530

    new search on change in input field

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,20 +3,22 @@ import React, { Component } from 'react'
 class SearchBar extends Component{
   constructor(props){
     super(props);
-    this.state = { term : 'Initial value' };
+    this.state = { term : '' };
     this.onInputChange = this.onInputChange.bind(this);
   }
 
   render(){
     return (
       <div className="search-bar">
-        <input value={this.state.term} onChange={this.onInputChange} />
+        <input value={this.state.term}
+               onChange={event=>{this.onInputChange(event.target.value)}} />
       </div>
     );
   }
 
-  onInputChange(event){
-    this.setState({ term :event.target.value });
+  onInputChange(term){
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -16,16 +16,20 @@ class App extends Component{
       videos : [],
       selectedVideo : null
     };
-    YTSearch({key:API_KEY, term : 'faydee'}, videos => this.setState({
+    this.videoSearch('faydee');
+
+  }
+
+  videoSearch(term){
+    YTSearch({key:API_KEY, term : term}, videos => this.setState({
       videos : videos,
       selectedVideo : videos[0]
     }));
-
   }
   render(){
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={ term=>{this.videoSearch(term)}} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo=>{this.setState({ selectedVideo })}}
